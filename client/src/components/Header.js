f268997e948a5bcb615452c3c875f1af2c8937b6
Elan Zinkow <ezinkow@gmail.com>
2021-03-24 14:42:40 -0500

    update links and put client in client folder

---
+++ client/src/components/Header.js
@@ -0,0 +1,120 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Avatar from '@material-ui/core/Avatar';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import HomeIcon from '@material-ui/icons/Home';
+import PersonIcon from '@material-ui/icons/Person';
+import InsertDriveFileIcon from '@material-ui/icons/InsertDriveFile';
+import WebIcon from '@material-ui/icons/Web';
+import EmailIcon from '@material-ui/icons/Email';
+import Zinkow_headshot from '../images/Zinkow_headshot.jpg'
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+  },
+  drawer: {
+    width: 250,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: 250,
+    background: '#3F3F3F',
+    color: 'white'
+  },
+  drawerContainer: {
+    overflow: 'auto',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+  icons: {
+    color: 'white',
+    margin: '10px 20px 10px 50px'
+  },
+  menu: {
+    color: 'white',
+    textDecoration: 'none'
+  },
+  avatar: {
+    margin: '15px auto',
+    display: 'block',
+    width: '150px',
+    height: '150px',
+    border: '8px solid #2c2f3f'
+  },
+  menuDiv: {
+    display: 'flex'
+  }
+
+}));
+
+export default function Header() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root} id="home">
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Elan Zinkow
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <Toolbar />
+        <div className={classes.drawerContainer}>
+          <List>
+            <Avatar alt="Zinkow" src={Zinkow_headshot} className={classes.large, classes.avatar} />
+            <h1 style={{ textAlign: 'center' }}>Elan Zinkow</h1>
+            <div style={{ textAlign: 'center' }}>
+              <a href="https://twitter.com/ElanZinkow" target="_blank" className='iconLink' style={{ size: '500px' }}><TwitterIcon /></a>
+              <a href="https://www.facebook.com/elan.zinkow" target="_blank" className='iconLink'><FacebookIcon /></a>
+              <a href="https://github.com/ezinkow" target="_blank" className='iconLink'><GitHubIcon /></a>
+              <a href="https://www.linkedin.com/in/elan-zinkow-b590ba5" target="_blank" className='iconLink'><LinkedInIcon /></a>
+            </div>
+          </List>
+          <List style={{ display: 'block' }}>
+            <div className={classes.menuDiv}>
+              <HomeIcon fontSize='large' className={classes.icons}/><a href="#home"><h2 className={classes.menu}>Home</h2></a>
+            </div>
+            <div className={classes.menuDiv}>
+              <PersonIcon fontSize='large' className={classes.icons}/><a href="#about"><h2 className={classes.menu}>About</h2></a>
+            </div>
+            <div className={classes.menuDiv}>
+              <InsertDriveFileIcon fontSize='large' className={classes.icons}/><a href="#resume"><h2 className={classes.menu}>Resumé</h2></a>
+            </div>
+            <div className={classes.menuDiv}>
+              <WebIcon fontSize='large' className={classes.icons}/><a href="#portfolio"><h2 className={classes.menu}>Portfolio</h2></a>
+            </div>
+            <a href="mailto:ezinkow@gmail.com" className={classes.menuDiv}>
+              <EmailIcon fontSize='large' className={classes.icons}/><h2 className={classes.menu}>Contact</h2>
+            </a>
+          </List>
+        </div>
+      </Drawer>
+    </div>
+  );
+}
---
+++ client/src/components/Portfolio.js
@@ -0,0 +1,195 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import puppies from '../images/puppies.jpg'
+import codingquiz from '../images/coding-quiz.jpg'
+import dayscheduler from '../images/day-scheduler.jpg'
+import hamburger from '../images/hamburger.jpg'
+import holidayatthemovies from '../images/holidayatthemovies.jpg'
+import notetaker from '../images/note-taker.jpg'
+import weatherdashboard from '../images/weather-dashboard.png'
+import basketballDb from '../images/basketballDb.jpg'
+import employeetracker from '../images/employee-tracker.jpg'
+import books from '../images/books-api-search.jpg'
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkIcon from '@material-ui/icons/Link';
+import snomore from '../images/snomore.jpg'
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+}));
+
+export default function Portfolio() {
+    const classes = useStyles();
+    return (
+        <div class="container">
+            <Grid container spacing={1}>
+                <Grid item xs={12} >
+                    <h1>Portfolio</h1>
+                    <h3>Find my projects below</h3>
+                </Grid>
+                <Grid item xs={6}>
+                    <img src={snomore} alt="Sno' More" style={{ width: '100%', height: '70%' }} />
+                    <div style={{ display: 'flex', textAlign: 'center' }}>
+                        <Grid item xs={4}>
+                            <h2 class="thumbName">Sno' More</h2>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <a href="https://github.com/ezinkow/SnoMore" target="_blank" rel="noreferrer" title="GitHub Repo" style={{ color: 'black' }}><h2><GitHubIcon fontSize="large" /></h2></a>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <a href="https://snow-more.herokuapp.com/" title="Sno' More" style={{ textDecoration: 'none', color: 'black' }}><h2><LinkIcon fontSize="large" /></h2><i
+                                class="bx bx-link"></i></a>
+                        </Grid >
+                    </div >
+                </Grid >
+                <Grid item xs={6}>
+                    <img src={basketballDb} alt="Basketball Stats Database" style={{ width: '100%', height: '70%' }} />
+                    <div style={{ display: 'flex', textAlign: 'center' }}>
+                        <Grid item xs={4}>
+                            <h2 class="thumbName">Basketball Stats Database</h2>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <a href="https://github.com/ezinkow/bball-stats-db" target="_blank" rel="noreferrer" title="GitHub Repo" style={{ color: 'black' }}><h2><GitHubIcon fontSize="large" /></h2></a>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <a href="https://ezinkow.github.io/bball-stats-db/" title="Basketball Stats Database" style={{ textDecoration: 'none', color: 'black' }}><h2><LinkIcon fontSize="large" /></h2><i
+                                class="bx bx-link"></i></a>
+                        </Grid >
+                    </div >
+                </Grid >
+                <Grid item xs={6}>
+                    <img src={puppies} alt="Basket of Puppies" style={{ width: '100%', height: '70%' }} />
+                    <div style={{ display: 'flex', textAlign: 'center' }}>
+                        <Grid item xs={4}>
+                            <h2 class="thumbName">Basket of Puppies</h2>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <a href="https://github.com/ezinkow/basket-of-puppies" target="_blank" rel="noreferrer" title="GitHub Repo" style={{ color: 'black' }}><h2><GitHubIcon fontSize="large" /></h2></a>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <a href="https://basket-of-puppies.herokuapp.com/" title="Basket of Puppies" style={{ textDecoration: 'none', color: 'black' }}><h2><LinkIcon fontSize="large" /></h2><i
+                                class="bx bx-link"></i></a>
+                        </Grid >
+                    </div >
+                </Grid >
+                <Grid item xs={6}>
+                    <img src={books} style={{ width: '100%', height: '70%' }} alt="Google Books Search" />
+                    <div style={{ display: 'flex', textAlign: 'center' }}>
+                        <Grid item xs={4}>
+                            <h2>Google Books API Search</h2>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <a href="https://github.com/ezinkow/google-books-search" target="_blank" rel="noreferrer" title="GitHub Repo"><GitHubIcon fontSize="large" /></a>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <a href="https://young-everglades-62991.herokuapp.com/" title="Google Books API Search"><LinkIcon fontSize="large" /></a>
+                        </Grid>
+                    </div>
+                </Grid>
+                <Grid item xs={6}>
+                    <img src={holidayatthemovies} style={{ width: '100%', height: '70%' }} alt="Holiday at the Movies" />
+                    <div style={{ display: 'flex', textAlign: 'center' }}>
+                        <Grid item xs={4}>
+                            <h2>Holiday At the Movies</h2>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <a href="https://github.com/ezinkow/Holiday-at-the-Movies" target="_blank" rel="noreferrer" title="GitHub Repo"><GitHubIcon fontSize="large" /></a>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <a href="https://ezinkow.github.io/Holiday-at-the-Movies/" title="Holiday At The Movies"><LinkIcon fontSize="large" /></a>
+                        </Grid>
+                    </div>
+                </Grid>
+                <Grid item xs={6}>
+                    <img src={employeetracker} style={{ width: '100%', height: '70%' }} alt="Employee Management System" />
+                    <div style={{ display: 'flex', textAlign: 'center' }}>
+                        <Grid item xs={5}>
+                            <h2>Employee Management System</h2>
+                        </Grid>
+                        <Grid item xs={3}>
+                            <a href="https://github.com/ezinkow/Employee-Tracker" target="_blank" rel="noreferrer" title="GitHub Repo"><GitHubIcon fontSize='large'/></a>
+                        </Grid>
+                        <Grid item xs={3}>
+                            <a href="https://youtu.be/jOBB4E-YE8Q" title="Employee Management System"><LinkIcon fontSize="large" /></a>
+                        </Grid>
+                    </div>
+                </Grid>
+                <Grid item xs={6}>
+                    <img src={hamburger} style={{ width: '100%', height: '70%' }} alt="Burger Logger" />
+                    <div style={{ display: 'flex', textAlign: 'center' }}>
+                        <Grid item xs={4}>
+                            <h2>Burger Logger</h2>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <a href="https://github.com/ezinkow/Burger-Logger" target="_blank" rel="noreferrer" title="GitHub Repo"><GitHubIcon fontSize='large'/></a>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <a href="https://shrouded-wildwood-93576.herokuapp.com/" title="Burger Logger"><LinkIcon fontSize="large" /></a>
+                        </Grid>
+                    </div>
+                </Grid>
+                <Grid item xs={6}>
+                    <img src={notetaker} style={{ width: '100%', height: '70%' }} alt="Note Taker" />
+                    <div style={{ display: 'flex', textAlign: 'center' }}>
+                        <Grid item xs={4}>
+                            <h2>Note Taker</h2>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <a href="https://github.com/ezinkow/Note-Taker" target="_blank" rel="noreferrer" title="GitHub Repo"><GitHubIcon fontSize='large'/></a>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <a href="https://note-taker-ez.herokuapp.com/notes" title="Note Taker"><LinkIcon fontSize="large" /></a>
+                        </Grid>
+                    </div>
+                </Grid>
+                <Grid item xs={6}>
+                    <img src={weatherdashboard} style={{ width: '100%', height: '70%' }} alt="Weather Dashboard" />
+                    <div style={{ display: 'flex', textAlign: 'center' }}>
+                        <Grid item xs={4}>
+                            <h2>Weather Dashboard</h2>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <a href="https://github.com/ezinkow/weather-dashboard" target="_blank" rel="noreferrer" title="GitHub Repo"><GitHubIcon fontSize='large'/></a>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <a href="https://ezinkow.github.io/weather-dashboard/" title="Weather Dashboard"><LinkIcon fontSize="large" /></a>
+                        </Grid>
+                    </div>
+                </Grid>
+                <Grid item xs={6}>
+                    <img src={codingquiz} style={{ width: '100%', height: '70%' }} alt="Coding Quiz" />
+                    <div style={{ display: 'flex', textAlign: 'center' }}>
+                        <Grid item xs={4}>
+                            <h2>Coding Quiz</h2>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <a href="https://github.com/ezinkow/coding-quiz" target="_blank" rel="noreferrer" title="GitHub Repo"><GitHubIcon fontSize='large'/></a>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <a href="https://ezinkow.github.io/coding-quiz/" title="More Details"><LinkIcon fontSize="large" /></a>
+                        </Grid>
+                    </div>
+                </Grid>
+
+                <Grid item xs={6}>
+                    <img src={dayscheduler} style={{ width: '100%', height: '70%' }} alt="Day Planner" />
+                    <div style={{ display: 'flex', textAlign: 'center' }}>
+                        <Grid item xs={4}>
+                            <h2>Day Planner</h2>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <a href="https://github.com/ezinkow/calendar-planner" target="_blank" rel="noreferrer" title="GitHub Repo"><GitHubIcon fontSize='large'/></a>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <a href="https://ezinkow.github.io/calendar-planner/" title="Day Planner"><LinkIcon fontSize="large" /></a>
+                        </Grid>
+                    </div>
+                </Grid>
+            </Grid >
+        </div >
+    )
+}
