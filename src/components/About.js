644df17ca26fc22389025b4b9fbd095ff7fbac45
Elan Zinkow <ezinkow@gmail.com>
2021-03-24 11:59:55 -0500

    updating about me

--- src/components/About.js
+++ src/components/About.js
@@ -7,9 +7,7 @@ export default function About() {
             I'm Elan. I live in the Uptown neighborhood in Chicago, IL with my wife and our two young boys. I have a
             strong background in professional sports production where I worked for 8 years. Please check out my <a
                 href="https://www.youtube.com/watch?v=DHQFKn39YyE&feature=emb_logo" target="_blank">reel</a> and my <a
-                    href="https://ezinkow.wixsite.com/elanzinkow" target="_blank">production website.</a> Currently, I'm a
-            coding bootcamp student through Northwestern University. Following completion of this intense program in
-            March, I plan on launching a new career as a software developer.
+                    href="https://ezinkow.wixsite.com/elanzinkow" target="_blank">production website.</a> I am a recent graduate of the Northwestern Coding Bootcamp program and now plan on launching a new career as a software developer.
         </Typography>
     )
 }
